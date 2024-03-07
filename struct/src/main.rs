9a824bcbe36040a5335768cd4c8708e4362c5783
Anton Kuzmin <thehappycoder@gmail.com>
2024-03-08 09:43:20 +1000

    struct

--- struct/src/main.rs
+++ struct/src/main.rs
@@ -4,23 +4,37 @@ enum Lang {
   Spanish,
   Chinese,
   Texan,
+  Russian,
+  Estonian,
 }
 
-struct Greeting {
-    message: String,
-    lang: Lang,
+struct Greeting<'a> {
+  message: &'a str,
+  lang: Lang,
 }
 
 fn main() {
-  let mut v :Vec<Greeting> = Vec::new();
+  let mut v: Vec<Greeting> = Vec::new();
 
-  let g : Greeting = Greeting { lang: Lang::English, message: String::from("Hello WasmEdge!") };
+  let g: Greeting = Greeting {
+    lang: Lang::English,
+    message: "Hello WasmEdge!",
+  };
   v.push(g);
-  let g : Greeting = Greeting { lang: Lang::Spanish, message: String::from("Hola WasmEdge!") };
+  let g: Greeting = Greeting {
+    lang: Lang::Spanish,
+    message: "Hola WasmEdge!",
+  };
   v.push(g);
-  let g : Greeting = Greeting { lang: Lang::Texan, message: String::from("Howdy WasmEdge!") };
+  let g: Greeting = Greeting {
+    lang: Lang::Texan,
+    message: "Howdy WasmEdge!",
+  };
   v.push(g);
-  let g : Greeting = Greeting { lang: Lang::Chinese, message: String::from("WasmEdge 你好!") };
+  let g: Greeting = Greeting {
+    lang: Lang::Chinese,
+    message: "WasmEdge 你好!",
+  };
   v.push(g);
 
   for e in v {
